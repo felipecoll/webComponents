314e3948f976e6bf495c44453d9514f3927309d5
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-29 19:33:58 -0300

    Web components - 2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Holaaa')
+const template = document.createElement('div')
+template.innerHTML = ` <style> p {color:blue}</style>
+<p>Hola mundoo!</p>
+<p>Ejemplo de agregar con template</p>`
+
+
+class myElement extends HTMLElement{
+ constructor() {
+    super()
+    
+    this.p = document.createElement('p')
+ }
+ connectedCallback() {
+    this.p.textContent = 'Hello World'
+    this.appendChild(this.p)
+    this.appendChild(template)
+ }
+}
+
+customElements.define('my-element', myElement)
